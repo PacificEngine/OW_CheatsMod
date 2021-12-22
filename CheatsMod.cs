89536af57b74d7c4c899c932bb0d652dd739297d
JoJo <joseph.salomone@dealertrack.com>
2021-12-21 17:37:57 -0800

    Small fix

--- CheatsMod.cs
+++ CheatsMod.cs
@@ -46,7 +46,7 @@ namespace PacificEngine.OW_CheatsMod
         Teleport_To_Ship,
         Teleport_To_Probe,
         Teleport_To_Nomai_Probe,
-        Teleport_To_BackerSatellite,
+        Teleport_To_Backer_Satellite,
         Teleport_Ship_To_Player,
         Toggle_Helmet,
         Toggle_Invinciblity,
@@ -163,7 +163,7 @@ namespace PacificEngine.OW_CheatsMod
             inputs.addInput(config, CheatOptions.Teleport_To_Nomai_Probe, "T,NumpadMinus");
             inputs.addInput(config, CheatOptions.Teleport_To_Vessel, "T,NumpadPlus");
             inputs.addInput(config, CheatOptions.Teleport_To_ProbeCannonCommandModule, "T,NumpadPeriod");
-            inputs.addInput(config, CheatOptions.Teleport_To_BackerSatellite, "T,B");
+            inputs.addInput(config, CheatOptions.Teleport_To_Backer_Satellite, "T,B");
 
             inputs.addInput(config, CheatOptions.Toggle_Anglerfish_AI, "V,I");
             inputs.addInput(config, CheatOptions.Toggle_Inhabitants_AI, "V,O");
@@ -316,7 +316,7 @@ namespace PacificEngine.OW_CheatsMod
                         case CheatOptions.Teleport_To_QuantumMoon:
                             Teleportation.teleportPlayerToQuantumMoon();
                             break;
-                        case CheatOptions.Teleport_To_BackerSatellite:
+                        case CheatOptions.Teleport_To_Backer_Satellite:
                             Teleportation.teleportPlayerToBackerSatellite();
                             break;
                         case CheatOptions.Toggle_Helmet:
